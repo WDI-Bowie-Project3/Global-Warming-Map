bbcd781df393840700676d8bc6642460693832cd
Patrick M <Patrick.M.Professional@gmail.com>
2016-03-17 15:30:13 -0400

    first commit in branch

---
+++ public/js/eventComponents/eventSearchBar.js
@@ -0,0 +1,8 @@
+'use strict'
+const React = require('react');
+
+const EventSearchBar = React.createClass({
+  //contextTypes : {}
+  //see login.js for more info if it ends up being required.
+
+})
