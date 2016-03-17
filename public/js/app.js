a1962b3d03b2d850e7d1cabc9dcd2f07cb187d50
Patrick M <Patrick.M.Professional@gmail.com>
2016-03-16 21:12:58 -0400

    first commit in 44_PatrickM_reactSkeleton

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+'use strict'
+const React = require('react');
+const ReactDOM = require('react-dom');
+const $ = require('jquery');
+
+
+
+
+
+
+
+
+ReactDOM.render( <App />, document.querySelector('#container') );
