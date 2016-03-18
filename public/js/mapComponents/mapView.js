46310ec8ad1e63df9c8e2c14103cb70cdfbdcaa5
Patrick M <Patrick.M.Professional@gmail.com>
2016-03-17 21:09:34 -0400

    test render of MapView worked.
    starting work on subcomponents.:

---
+++ public/js/mapComponents/mapView.js
@@ -0,0 +1,17 @@
+'use strict'
+const React = require('react');
+
+
+const MapView = React.createClass({
+
+
+  render : function () {
+
+
+    return (
+      <p>test render of MapView</p>
+    )
+  }
+})
+
+module.exports = MapView;
